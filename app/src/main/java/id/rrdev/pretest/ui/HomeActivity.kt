593bd854cbcb8a4e0b2ac136c752464e528ae0ed
Rasyid <rasydridla298@gmail.com>
2021-12-21 13:46:50 +0700

    layouting UI

---
+++ app/src/main/java/id/rrdev/pretest/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package id.rrdev.pretest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.rrdev.pretest.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
