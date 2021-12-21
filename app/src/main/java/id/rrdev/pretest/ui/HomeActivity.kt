871b21471463d659400d578fd833d1ce1bd2efba
Rasyid <rasydridla298@gmail.com>
2021-12-21 13:58:17 +0700

    setup view binding

--- app/src/main/java/id/rrdev/pretest/ui/HomeActivity.kt
+++ app/src/main/java/id/rrdev/pretest/ui/HomeActivity.kt
@@ -1,12 +1,56 @@
 package id.rrdev.pretest.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import id.rrdev.pretest.R
+import id.rrdev.pretest.databinding.ActivityHomeBinding
+import id.rrdev.pretest.ui.about.AboutFragment
+import id.rrdev.pretest.ui.product.ProductFragment
+import id.rrdev.pretest.ui.report.ReportFragment
+import id.rrdev.pretest.ui.transaction.TransactionFragment
 
 class HomeActivity : AppCompatActivity() {
+
+    private val binding: ActivityHomeBinding by lazy {
+        ActivityHomeBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        navigationChange(ProductFragment())
+
+        binding.bottomNavigationContainer.setOnNavigationItemSelectedListener {item ->
+            when (item.itemId) {
+                R.id.menu_product -> {
+                    navigationChange(ProductFragment())
+                }
+                R.id.menu_transaction  -> {
+                    navigationChange(TransactionFragment())
+                }
+                R.id.menu_report  -> {
+                    ReportFragment()
+                }
+                R.id.menu_about  -> {
+                    AboutFragment()
+                }
+            }
+            false
+        }
+    }
+
+    private fun navigationChange(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameContainer, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .commit()
     }
 }
--- app/src/main/java/id/rrdev/pretest/ui/login/LoginActivity.kt
+++ app/src/main/java/id/rrdev/pretest/ui/login/LoginActivity.kt
@@ -3,10 +3,17 @@ package id.rrdev.pretest.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.rrdev.pretest.R
+import id.rrdev.pretest.databinding.ActivityHomeBinding
+import id.rrdev.pretest.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
